2fb6ae8f4a2a8782d9aa93660e68f772a03ac1c4
anusha020515 <anushajangra1504@gmail.com>
2022-05-17 14:19:18 +0530

    added cpp files

---
+++ 1+11+111+1111+11111+111111=123456.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int a,b,c=0,n=0,d,i=0;
+	cin>>a;
+	for(i=1;i<=a;i=i+1)
+	{
+		
+	
+	c=c*10+1;
+	n=n+c;
+		cout<<c;
+	if(i<a)
+	{
+		cout<<"+";
+		
+	}
+	else
+	{
+		cout<<"=";
+		
+	}
+
+
+	
+
+}
+cout<<n;
+
+}
+
---
+++ if.... else condition.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	
+	int a;
+	cout<<"the number=";
+	cin>>a;
+	int b;
+	if (a%2==0	)
+	{
+		cout<<"number is even ";
+		 
+	}
+else
+	{
+		cout<<"number is odd";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
